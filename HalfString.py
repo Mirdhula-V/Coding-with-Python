b0302564229e179ef38d871ce8ae76fd32945721
Mirdhula-V <vmirdhula04@gmail.com>
2024-10-05 18:43:24 +0530

    Coding for Separaring half string

---
+++ HalfString.py
@@ -0,0 +1,8 @@
+word = input()
+length_of_word =  len(word)
+length_of_word = int(length_of_word)
+half_part = length_of_word/2
+half_part = int(half_part)
+new_word = word[half_part:]
+
+print(new_word)
